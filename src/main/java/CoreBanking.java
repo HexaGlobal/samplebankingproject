ddd96e8c62d8cbb7e7df6a5b5ee5415560f1acfa
HexaGlobalSolutions <92368863+HexaGlobal@users.noreply.github.com>
2021-10-27 15:54:00 +0530

    First commit

---
+++ src/main/java/CoreBanking.java
@@ -0,0 +1,13 @@
+
+public class CoreBanking {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hey.. Thanks for visiting HexaglobalSolutions.com "
+				+ ""
+				+ "We are now pushing file to github from STS IDE.");
+    
+
+	}
+
+}
